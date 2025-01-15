736bc6b72825bddbb5c1c10e67f7bad1572a3af7
zhengjie.vendor <zhengjie.vendor@sensetime.com>
2025-01-15 18:55:44 +0800

    feat:组件传递，父传子

--- react-study/src/App.tsx
+++ react-study/src/App.tsx
@@ -1,29 +1,26 @@
 const Button = () => {
   return (
-    // 下面的写法实际上直接等价于<button>Click me</button>
     <>
       <button>Click me</button>
     </>
   );
 };
 
-const User = () => {
+const User = (props: any) => {
+  console.log(props);
+
   return (
     <div>
       <h1>hello</h1>
-      <h2>world</h2>
+      <h2>{props.userInfo.name}</h2>
     </div>
   );
 };
 
-// JSX语法不支持直接返回多个元素，但是可以使用Fragment组件包裹多个元素，Fragment组件可以简写为<></>。
 function App() {
   return (
     <>
-      <div className="app">
-        <Button></Button>
-      </div>
-      <User></User>
+      <User userInfo={{ name: "jie" }}></User>
     </>
   );
 }
