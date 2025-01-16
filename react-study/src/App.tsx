f9dd2c70fca397c4fba1075c4e3beb0ad8a30b75
zhengjie.vendor <zhengjie.vendor@sensetime.com>
2025-01-16 11:35:57 +0800

    feat:子传父

--- react-study/src/App.tsx
+++ react-study/src/App.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import "./User.css";
+
 const Button = () => {
   return (
     <>
@@ -6,21 +9,44 @@ const Button = () => {
   );
 };
 
-const User = (props: any) => {
+type UserProps = {
+  userInfo: {
+    name: string;
+    message: string;
+  };
+  onMessageChange: (newMessage: string) => void;
+};
+
+// 编写样式
+
+const User = (props: UserProps) => {
   console.log(props);
+  const setNewMsg = () => {
+    props.onMessageChange("我是子组件传递给父组件的消息");
+  };
 
   return (
-    <div>
+    <div className="user">
       <h1>hello</h1>
       <h2>{props.userInfo.name}</h2>
+      <button onClick={setNewMsg}>Click me</button>
     </div>
   );
 };
 
 function App() {
+  const [message, setMessage] = useState<string>("hello , my name is jie!");
+
+  const handleMessageChange = (newMsg: string) => {
+    setMessage(newMsg);
+  };
   return (
     <>
-      <User userInfo={{ name: "jie" }}></User>
+      <User
+        userInfo={{ name: "jie", message: message }}
+        onMessageChange={handleMessageChange}
+      ></User>
+      <h2>{message}</h2>
     </>
   );
 }
